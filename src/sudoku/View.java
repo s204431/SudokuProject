93d60754a2cced97199a5e289ae77b6d7cdc4574
michaelfn <michaelfognielsen@hotmail.com>
2022-02-09 15:01:59 +0100

    Added frame, panel, fields in View

--- src/sudoku/View.java
+++ src/sudoku/View.java
@@ -4,24 +4,30 @@ import javax.swing.*;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
 
-public class View {
+public class View extends JFrame {
 
-    public View(){
-        JTextField searchTermTextField = new JTextField(26);
+    public View() {
+        // Create panel
         JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
-        JButton button = new JButton("Button");
-        JLabel label = new JLabel("This is a sudoku solver");
-        // Display it all in a scrolling window and make the window appear
-        JFrame frame = new JFrame("Sudoku-Solver");
-        panel.add(label);
-        panel.add(button);
+        panel.setLayout(new GridLayout(9,9));
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.add(panel);
-        frame.setSize(500,500);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+        // Add fields
+        JTextField field;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                field = new JTextField(1);
+                field.setHorizontalAlignment(JTextField.CENTER);
+                panel.add(field);
+            }
+        }
+
+        // Add frame
+        setTitle("Sudoku");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        add(panel);
+        setSize(500,500);
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
 }
