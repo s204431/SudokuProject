4adc993d526e65af9ba86f35ec45c11a487aaee8
Gideon <gidewadejob@gmail.com>
2022-02-09 14:34:40 +0100

    basic ui made

--- src/sudoku/Main.java
+++ src/sudoku/Main.java
@@ -1,7 +1,8 @@
+package sudoku;
 
 public class Main {
-
 	public static void main(String[] args) {
 		System.out.println("Hello world.");
+		View view = new View();
 	}
 }
--- src/sudoku/View.java
+++ src/sudoku/View.java
@@ -1,5 +1,27 @@
 package sudoku;
 
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+
 public class View {
 
+    public View(){
+        JTextField searchTermTextField = new JTextField(26);
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        JButton button = new JButton("Button");
+        JLabel label = new JLabel("This is a sudoku solver");
+        // Display it all in a scrolling window and make the window appear
+        JFrame frame = new JFrame("Sudoku-Solver");
+        panel.add(label);
+        panel.add(button);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.add(panel);
+        frame.setSize(500,500);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
