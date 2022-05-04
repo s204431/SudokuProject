5614cf152137627eba568a072a5e6fd85f80708d
michaelfn <michaelfognielsen@hotmail.com>
2022-05-04 14:54:19 +0200

    Labels above your and your opponent's board

--- src/main/java/multiplayer/MultiplayerView.java
+++ src/main/java/multiplayer/MultiplayerView.java
@@ -66,8 +66,19 @@ public class MultiplayerView extends View {
 		opponentPanel.repaint();
         super.paint(g);
 
+		// Text clarifying which board is which
+		g2.setFont(new Font("Serif", Font.BOLD, 50));
+		int textWidth = (windowWidth-buttonPanel.getWidth()) / 4 - g2.getFontMetrics().stringWidth("You") / 2;
+		int textHeight = g2.getFontMetrics().getHeight();
+		int offSet = 10;
+		g2.setColor(buttonPanel.getBackground());
+		g2.fillRect(0, 0, windowWidth, textHeight + offSet);
+		g2.setColor(Color.BLACK);
+		g2.drawString("You", textWidth, textHeight - offSet);
+
+		// Line seperating the two boards
 		g2.drawLine((getPreferredSize().width - 200) / 2 - 1, 0, (getPreferredSize().width - 200) / 2 - 1, getPreferredSize().height);
-    }
+	}
 
 	public void resetBoardPosition() {
     	fieldWidth = (int)((Field.DEFAULT_WIDTH*windowWidth / 1170.0) / 1.5);
@@ -209,6 +220,18 @@ public class MultiplayerView extends View {
 				}
 			}
 			g2.setStroke(oldStroke);
+
+			g2.setFont(new Font("Serif", Font.BOLD, 50));
+			int textWidth = (windowWidth-buttonPanel.getWidth()) / 4 - g2.getFontMetrics().stringWidth("Opponent") / 2;
+			int textHeight = g2.getFontMetrics().getHeight();
+			int offSet = 10;
+			g2.setColor(buttonPanel.getBackground());
+			g2.fillRect(0, 0, windowWidth, textHeight + offSet);
+			g2.setColor(Color.BLACK);
+			g2.drawString("Opponent", textWidth, textHeight - offSet);
+
+			//g2.setFont(new Font("Serif", Font.BOLD, 50));
+			//g2.drawString("Opponent", (windowWidth-buttonPanel.getWidth()) / 4 - g2.getFontMetrics().stringWidth("Opponent") / 2, g2.getFontMetrics().getHeight());
 
 			// Draw components
 			//buttonPanel.repaint();
